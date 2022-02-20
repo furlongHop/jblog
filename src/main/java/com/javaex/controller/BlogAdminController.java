67175934d05ed3af0bb84ddd4a1165d9005a35e0
황지우 <skyro13@naver.com>
2022-02-20 22:30:46 +0900

    blogAdmin

--- src/main/java/com/javaex/controller/BlogAdminController.java
+++ src/main/java/com/javaex/controller/BlogAdminController.java
@@ -1,11 +1,20 @@
 package com.javaex.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.service.BlogService;
+import com.javaex.vo.BlogVo;
 
 @Controller
 @RequestMapping("/{id}/admin")
@@ -13,16 +22,36 @@ public class BlogAdminController {
 
 	@Autowired
 	private BlogService blogService;
-	
+
 	@RequestMapping("/basic")
 	public String basic(@PathVariable("id") String id) {
 		System.out.println("BlogAdminController/basic()");
-		
-		
-		
+
 		return "blog/admin/blog-admin-basic";
 	}
 
+	@RequestMapping("/writeForm")
+	public String writeForm() {
+		System.out.println("BlogAdminController/wirteForm()");
 
-	
+		return "blog/admin/blog-admin-write";
+	}
+
+	@RequestMapping("/write")
+	public String write() {
+		System.out.println("BlogAdminController/wirte()");
+		
+		return "redirect:writeForm";
+	}
+
+
+	@RequestMapping("/category")
+	public String category() {
+		System.out.println("BlogAdminController/category()");
+
+
+		return "blog/admin/blog-admin-cate";
+	}
+
+
 }
